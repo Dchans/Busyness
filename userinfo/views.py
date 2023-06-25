5bdb63a4b1088f9d6077c99941db0f835dacf8e2
Dchans <135418178+Dchans@users.noreply.github.com>
2023-06-25 11:12:47 +0530

    Update views.py

--- userinfo/views.py
+++ userinfo/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import FileResponse
 def home_page(request):
     return render(request,'homepage.html')
+def download_maindb(request):
+     with open("backup.db", 'rb') as file:
+         file_name = "main.db"
+         response = FileResponse(file, content_type='text/plain')
+         response['Content-Disposition'] = 'attachment; filename="main.db"'
+         return response
+def download_maindb(request):
+     with open("backup.db", 'rb') as file:
+         file_name = "backup.db"
+         response =  FileResponse(file, content_type='text/plain')
+         response['Content-Disposition'] = 'attachment; filename="backup.db"'
+         return response
 
    
 
