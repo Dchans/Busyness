d84d1dc521d9f7cda370faf04b6d395944c7e485
Dchans <135418178+Dchans@users.noreply.github.com>
2023-06-25 10:02:10 +0530

    Update admin.py

--- userinfo/admin.py
+++ userinfo/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from userinfo.models import userdata,phone_verification,email_verification,usernews
+from userinfo.models import userdata,phone_verification,email_verification,usernews,FileModel
 admin.site.register(userdata)
 admin.site.register(phone_verification)
 admin.site.register(email_verification)
 admin.site.register(usernews)
-# Register your models here.
-
-# Register your models here.
+admin.site.register(FileModel)
