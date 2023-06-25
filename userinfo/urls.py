74a203f57a26ba32315508fd1df95f8d6570b182
Dchans <135418178+Dchans@users.noreply.github.com>
2023-06-25 11:14:06 +0530

    Update urls.py

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from userinfo.views import home_page
+from userinfo.views import home_page,download_maindb,download_backupdb
 urlpatterns = [
     path('',home_page,name="home"),
+    path('backupdb',home_page,page_name="download_bd"),
+     path('backupdb',home_page,page_name="download_bd"),
 ]
