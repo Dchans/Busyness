673b5e9fa37ab5e602d3de1ae5dc6f6818906c66
Dchans <135418178+Dchans@users.noreply.github.com>
2023-07-07 22:05:08 +0530

    Update settings.py

--- busyness/settings.py
+++ busyness/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'busyness.wsgi.application'
 DATABASES={
     'default':{
         'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'dchans',
+        'NAME':'busyness',
         'USER':'devendran',
         'PASSWORD':'dchan@9120',
         'HOST':'localhost',
